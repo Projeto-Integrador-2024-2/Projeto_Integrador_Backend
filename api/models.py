1085c6de388d7534eb6fd48da558552df8feb479
Rafael Machado Wanner <rafamwanner@gmail.com>
2024-12-15 01:46:38 -0300

    mudado o model do choice pra ser forein key ao em vez de one to one

--- api/models.py
+++ api/models.py
@@ -31,7 +31,7 @@ class Scene(models.Model):
 class Choice(models.Model):
     text = models.CharField(max_length=255, default="")
     from_scene = models.ForeignKey(Scene, default='', on_delete=models.CASCADE, related_name='from_scene')
-    to_scene = models.OneToOneField(Scene, null=True, on_delete=models.CASCADE, related_name='to_scene')
+    to_scene = models.ForeignKey(Scene, null=True, on_delete=models.CASCADE, related_name='to_scene')
 
 class Description(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="description")
